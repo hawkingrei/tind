0ff2cc220dc06c00b14f3b20bb22f35362d902d8
Weizhen Wang <wangweizhen@pingcap.com>
2021-08-26 11:54:19 +0800

    support more tidb version and update Dockerfile

--- build.sh
+++ build.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 set -x
-TIDB_VERSION_SLICE=('v5.1.1' 'v5.1.0' 'v5.0.3' 'v4.0.14')
+TIDB_VERSION_SLICE=(
+    'v5.1.1' 
+    'v5.1.0' 
+    'v5.0.3' 
+    'v4.0.14'
+    'v4.0.13'
+    'v4.0.12'
+    'v4.0.11'
+    'v4.0.10'
+    'v4.0.9'
+    'v4.0.8'
+    'v4.0.7'
+    'v4.0.6'
+    'v4.0.5'
+    'v4.0.4'
+    'v4.0.3'
+    'v4.0.2'
+    'v4.0.1'
+    'v4.0.0'
+)
  
 for version in "${TIDB_VERSION_SLICE[@]}"
 do
