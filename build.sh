42dc267a186aa3d5100703a8cacd388799d6cb94
Weizhen Wang <wangweizhen@pingcap.com>
2021-09-14 16:58:47 +0800

    add --no-cache for dockerfile

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -x
 TIDB_VERSION_SLICE=(
+    'v5.2.1'
     'v5.2.0'
     'v5.1.1' 
     'v5.1.0' 
@@ -26,6 +27,6 @@ for version in "${TIDB_VERSION_SLICE[@]}"
 do
     export TIDB_VERSION=${version}
     echo ${version}
-	docker build -t hawkingrei/tind:${TIDB_VERSION}  --build-arg TIDB_VERSION .
+	docker build --no-cache -t hawkingrei/tind:${TIDB_VERSION}  --build-arg TIDB_VERSION .
     docker push hawkingrei/tind:${TIDB_VERSION}
 done
