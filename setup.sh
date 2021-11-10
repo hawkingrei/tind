1772d74dfc22abce2016a2dbf9e55df3d2ac3c35
Weizhen Wang <wangweizhen@pingcap.com>
2021-11-10 17:32:34 +0800

    update dependency
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- setup.sh
+++ setup.sh
@@ -4,9 +4,8 @@ set -eu
 . /etc/os-release
 
 apt-get update
-apt-get install -y gnupg
-echo "deb http://ppa.launchpad.net/deadsnakes/ppa/ubuntu ${UBUNTU_CODENAME} main" > /etc/apt/sources.list.d/deadsnakes.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F23C5A6CF475977595C89F51BA6932366A755776
+apt-get install -y gnupg software-properties-common
+add-apt-repository ppa:deadsnakes/ppa
 
 apt-get update
 apt-get install -y \
@@ -14,19 +13,15 @@ apt-get install -y \
     python3-pip \
     python3-distutils \
     python3-setuptools \
-    \
     python2.? \
     python2.?-dev \
     python3.? \
     python3.?-dev \
-    python3.?-venv \
-    python2.10 \
-    python2.10-dev \
+    python3.?-venv \ 
     python3.10 \
     python3.10-dev \
-    python3.10-venv \    
-    supervisor \
-    ;
+    python3.10-venv \
+    supervisor 
 
 pip3 install \
     flit \
